89b0d976c875e18b0831805a9d8f9f629b3a1484
chokil <chokilitte@yahoo.co.jp>
2022-12-13 14:54:16 +0900

    delete export default

--- pages/about.js
+++ pages/about.js
@@ -1,8 +1,8 @@
 import Head from "next/head";
 import Image from "next/image";
 import { Footer } from "../components/Footer";
-import Headline from "../components/Headline";
-import Links from "../components/Links";
+import { Headline } from "../components/Headline";
+import { Links } from "../components/Links";
 import styles from "../styles/Home.module.css";
 
 export default function Home() {
@@ -15,7 +15,7 @@ export default function Home() {
       </Head>
 
       <main className={styles.main}>
-        <Headline page="about" />
+        <Headline title="About page" page="about" />
 
         <Links />
       </main>
